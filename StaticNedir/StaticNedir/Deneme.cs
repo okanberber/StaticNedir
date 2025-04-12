d7ae900402c472be243c9324023f1ff72c44344e
Okan Berber <okan.berber@outlook.com.tr>
2025-04-12 15:51:37 +0300

    Project Completed

---
+++ StaticNedir/StaticNedir/Deneme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticNedir
+{
+    public class Deneme
+    {
+        public static void Yazdir()
+        {
+            Console.WriteLine("Selam");
+        }
+    }
+}
---
+++ StaticNedir/StaticNedir/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticNedir
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Deneme.Yazdir();
+
+            //Program p = new Program();
+            //p.SelamVer();
+            SelamVer();
+        }
+        public static void SelamVer()
+        {
+            Console.WriteLine("Abi Selamlar");
+        }
+    }
+}
